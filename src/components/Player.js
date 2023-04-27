f97a9e3117c98be1a202fd82478062da2d2402da
Bjay <jayanjay2018@gmail.com>
2023-04-27 17:00:09 +0300

    Added The Seek Bar

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"; 
 import useSound from "use-sound"; // for handling the sound
-// import qala from "../assets/qala.mp3"; // importing the music
+import qala from "../assets/qala.mp3"; // importing the music
 import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai"; // icons for play and pause
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi"; // icons for next and previous track
 import { IconContext } from "react-icons"; // for customazing the icons
@@ -9,7 +9,38 @@ import "./Player.css"
 
 const Player = () => {
     const [isPlaying, setIsPlaying] = useState(false);
-    const [play, { pause, duration, sound }] = useSound("")
+    const [play, { pause, duration, sound }] = useSound(qala)
+    const [currTime, setCurrTime] = useState({
+        min: "",
+        sec: "",
+      }); // current position of the audio in minutes and seconds
+    
+      const [seconds, setSeconds] = useState(); // current position of the audio in seconds
+
+      useEffect(() => {
+        const sec = duration / 1000;
+        const min = Math.floor(sec / 60);
+        const secRemain = Math.floor(sec % 60);
+        const time = {
+          min: min,
+          sec: secRemain,
+        }});
+
+        useEffect(() => {
+            const interval = setInterval(() => {
+              if (sound) {
+                setSeconds(sound.seek([])); // setting the seconds state with the current state
+                const min = Math.floor(sound.seek([]) / 60);
+                const sec = Math.floor(sound.seek([]) % 60);
+                setCurrTime({
+                  min,
+                  sec,
+                });
+              }
+            }, 1000);
+            return () => clearInterval(interval);
+          }, [sound]);
+            
 
     const playingButton = () => {
         if (isPlaying) {
@@ -57,6 +88,27 @@ const Player = () => {
             <BiSkipNext />
           </IconContext.Provider>
         </button>
+      </div>
+      <div>
+        <div className="time">
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+          <p>
+            {currTime.min}:{currTime.sec}
+          </p>
+        </div>
+        <input
+          type="range"
+          min="0"
+          max={duration / 1000}
+          default="0"
+          value={seconds}
+          className="timeline"
+          onChange={(e) => {
+            sound.seek([e.target.value]);
+          }}
+        />
       </div>
     </div>
     
