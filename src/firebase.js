dec32eb9cbd8a64aa107a1fa9cca299673e4d2ee
Bjay <jayanjay2018@gmail.com>
2023-04-29 20:00:15 +0300

    Firebase updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,20 @@
-// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBZMkw2Vgpg2TmoVNf6mcKLW0Z-_umx_vI",
-  authDomain: "beats-by-jay.firebaseapp.com",
-  projectId: "beats-by-jay",
-  storageBucket: "beats-by-jay.appspot.com",
-  messagingSenderId: "1010813318297",
-  appId: "1:1010813318297:web:bbffbc3db0aa67597db408",
-  measurementId: "G-1ZCLC5Y3ZH"
-};
-firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyBZMkw2Vgpg2TmoVNf6mcKLW0Z-_umx_vI",
+    authDomain: "beats-by-jay.firebaseapp.com",
+    projectId: "beats-by-jay",
+    storageBucket: "beats-by-jay.appspot.com",
+    messagingSenderId: "1010813318297",
+    appId: "1:1010813318297:web:bbffbc3db0aa67597db408",
+    measurementId: "G-1ZCLC5Y3ZH"
+  };
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-// Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db,auth }
