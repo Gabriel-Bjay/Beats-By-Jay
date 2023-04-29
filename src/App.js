12ec94f954f6ea8e241ceda9256ae7525364b31e
Bjay <jayanjay2018@gmail.com>
2023-04-29 12:35:34 +0300

    Added A sign Up page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Nav from './components/Navbar/Navigation';
 import Player from './components/Player';
 
 function App() {
   return (
     <div className="App">
       <Player/>
+      <Nav/>
     </div>
   );
 }
